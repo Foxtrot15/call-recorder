a8e874b32eaf60cef1dd4b6206b1b25044e12666
Foxtrot15 <133854676+Foxtrot15@users.noreply.github.com>
2026-06-29 19:50:39 +1000

    Update sms.js

--- src/services/sms.js
+++ src/services/sms.js
@@ -1,5 +1,4 @@
 const twilio = require("twilio");
-
 const client = twilio(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_AUTH_TOKEN
@@ -7,8 +6,8 @@ const client = twilio(
 
 async function sendSMS(body) {
   await client.messages.create({
-    from: "whatsapp:+14155238886",
-    to:   `whatsapp:${process.env.TRANSCRIPT_RECIPIENT_NUMBER}`,
+    from: process.env.TWILIO_NUMBER,
+    to:   process.env.TRANSCRIPT_RECIPIENT_NUMBER,
     body,
   });
 }
